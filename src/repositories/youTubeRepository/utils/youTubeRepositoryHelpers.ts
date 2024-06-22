e878a2574e233c0c3fc0b10bce21047ca86176c6
FuyaKoshiro <s.castle28546@gmail.com>
2024-06-21 20:22:09 -0700

    Fix transforming function

--- src/repositories/youTubeRepository/utils/youTubeRepositoryHelpers.ts
+++ src/repositories/youTubeRepository/utils/youTubeRepositoryHelpers.ts
@@ -1,9 +1,18 @@
 export function convertSpecialCharactersToNormal(text: string): string {
+  let hasSpecialCharacters = true;
+
+  while (hasSpecialCharacters) {
+    const originalText = text;
     text = text.replace(/&amp;/g, "&");
     text = text.replace(/&lt;/g, "<");
     text = text.replace(/&gt;/g, ">");
     text = text.replace(/&quot;/g, '"');
-    text = text.replace(/&#039;/g, "'");
-  
-    return text;
+    text = text.replace(/&#39;/g, "'");
+
+    if (text === originalText) {
+      hasSpecialCharacters = false;
+    }
   }
+
+  return text;
+}
--- src/repositories/youTubeRepository/youTubeRepository.ts
+++ src/repositories/youTubeRepository/youTubeRepository.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { YoutubeTranscript, TranscriptResponse } from "youtube-transcript";
+import { YoutubeTranscript } from "youtube-transcript";
 import {
   Language,
   transcriptSchema,
